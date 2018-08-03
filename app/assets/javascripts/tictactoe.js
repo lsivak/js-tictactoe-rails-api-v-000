d7af09961b1af5e1f00ce098fc6110463d600e8a
Lara Sanders <larasanders@gmail.com>
2018-08-03 21:53:14 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,26 @@
 // Code your JavaScript / jQuery solution here
+
+let player1 = "O"
+let player2 = "X"
+let turn = 1
+function player(turn) {
+	if(turn%2 === true);
+		return 'X';
+	if(turn%2 === false);
+		return 'O';
+	
+}
+
+window.attachListeners = () => {
+	function doTurn(player) {
+		turn += 1
+	}
+}
+function updateState() {
+  var square = document.getElementById("data-x", "data-y");
+  square = player(turn);
+var el = document.getElementById("games");
+el.addEventListener("click", () => { updateState(square); }, false);
+}
+
+
