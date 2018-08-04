6c1c70b5ddbb60ee3583595ba57fd97feb14ea23
Lara Sanders <larasanders@gmail.com>
2018-08-04 17:21:13 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,9 +1,29 @@
 // Code your JavaScript / jQuery solution here
+const squares = window.document.querySelectorAll('td');
+const messageDiv = window.document.getElementById('message');
+const gamesDiv = window.document.getElementById('games');
+const saveButton = window.document.getElementById('save');
+const previousButton = window.document.getElementById('previous');
+const clearButton = window.document.getElementById('clear');
 
-let player1 = "X"
-let player2 = "O"
+function resetFixtures() {
+  for (let i = 0; i < 9; i++) {
+    squares[i].innerHTML = '';
+  }
+}
+  window.turn *= 0;
+  messageDiv.innerHTML = '';
+  gamesDiv.innerHTML = '';
 
-function player(turn) {
+
+function populateBoard(arr) {
+  for (let i = 0; i < 9; i++) {
+    squares[i].innerHTML = arr[i];
+  }
+}
+
+
+function player() {
 	if (turn%2 === 0) {
 		return 'X';
 	} else {
@@ -12,7 +32,7 @@ function player(turn) {
 }
 function attachListeners() {
 	var el = document.getElementById("squares");
-	el.addEventListener("click", () => { doTurn().innerHTML; }, false);
+	el.addEventListener("click", () => { doTurn(player).innerHTML; }, false);
 }
 
 function doTurn(player) {
@@ -24,13 +44,14 @@ function doTurn(player) {
 function setMessage(checkWinner) {
 	if (checkWinner == true) {
 	return "Player #{player} Won!"
+	resetFixtures()
 }
 }
 function updateState() {
   const squares = window.document.querySelectorAll('td');
-  square = player(turn);
+  square = player();
 var el = document.getElementById("games");
-el.addEventListener("click", () => { updateState(squares).innerHTML; }, false);
+el.addEventListener("click", () => { updateState(squares[i]).innerHTML; }, false);
 
 $(document).ready (function () {
 $(".js-save-game").on("click", function(e) { 
@@ -41,12 +62,17 @@ $(".js-save-game").on("click", function(e) {
 
 
 function checkWinner() {
-	const squares = window.document.querySelectorAll('tr')
+	const squares = window.document.querySelectorAll('td')
+// if (tr[0][0] == tr[0][1] == tr[0][2] || tr[1][0] == tr[1][1] == tr[1][2] || tr[2][0] == tr[2][1] == tr[2][2]  ) {
+if (squares[0].innerHTML==squares[1].innerHTML && squares[1].innerHTML==squares[2].innerHTML || squares[3].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[5].innerHTML || squares[6].innerHTML==squares[7].innerHTML && squares[7].innerHTML==squares[8].innerHTML ||
+squares[0].innerHTML==squares[3].innerHTML && squares[3].innerHTML==squares[6].innerHTML || squares[1].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[7].innerHTML || squares[2].innerHTML==squares[5].innerHTML && squares[5].innerHTML==squares[8].innerHTML ||
+squares[0].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[8].innerHTML || squares[2].innerHTML==squares[4].innerHTML && squares[4].innerHTML==squares[6].innerHTML) {
 
-if (squares[0]==squares[1] && squares[1]==squares[2] || squares[3]==squares[4] && squares[4]==squares[5] || squares[6]==squares[7] && squares[7]==squares[8]) {
 return true;
+} else {
+	return false;
+}
+setMessage()
+}
 
-	setMessage()
-}
-}
 
