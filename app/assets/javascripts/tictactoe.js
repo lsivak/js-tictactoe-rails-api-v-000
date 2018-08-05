d88c1b8671e7d7af8c27e4a9a9db41664b79ac5b
Lara Sanders <larasanders@gmail.com>
2018-08-05 14:56:57 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -7,15 +7,28 @@ var turn = 0
 var player = () => turn % 2 ? 'O' : 'X';
 
 function attachListeners() {
-	// var el = document.getElementById("td");
-	// el.addEventListener("click", () => {
-
+ 
+ // el.addEventListener("click", function(e) {
+  // $('td').text((index, square) => {
+ // 	document.querySelectorAll("td").forEach(function(square) {
+ // 		square.addEventListener("click", function() {
+ // 			doTurn(this)
+	// 	});
+	// 	});
+   $('#games').on('click', function() {
+		doTurn(this);
+		})
+		var el = document.getElementById("#button");
 	$('#save').on('click', () => saveGame());
 	$('#previous').on('click', () => showPreviousGames());
 	$('#clear').on('click', () => resetBoard());
 
 }
-
+// }
+// })
+// 
+// doTurn(square)
+// }
 
 function doTurn(square) {
 		updateState(square);
@@ -27,6 +40,7 @@ function doTurn(square) {
 		message = "Tie game.";
 		setMessage(message);
 		clearBoard()
+		currentGame = 0
 }
 }
 
@@ -43,12 +57,42 @@ function clearBoard() {
 	$('td').remove();
 	turn = 0
 }
-
+function gameId() {
+var getGameId = function(event) {	
+  return $(event.target).data("gameid")	
+}
+}
 function saveGame() {
-	var games = []
-	 $('td').text((index, square) => {
-		 state.push(games);
+	var gameInfo = []
+	$('td').text((index, square) => {
+		gameInfo.push(this);
 });
+
+var gameData = {gameInfo: state}
+if (currentGame) {
+	$.ajax({
+		type: 'PATCH',
+		url: `/games/${currentGame}`,
+		data: gameData
+	})
+} else {
+	$.post('/games', gameData, function(game) {
+		currentGame = game.data.id;
+		$('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`)
+		$("#gameid-" + game.data.id).on('click', () => showGame(game))
+	})
+}
+}
+	// $("#games").click(function(event) {
+	// 	gameId(event)
+	// })
+	// $("#save").click(function() {
+	// 	save()
+	// })
+// 
+// }
+function showGame(game) {
+	return $('<li>', {'data-state': game.state, 'data-gameid': game.id, text: game.id})
 }
 
 function checkWinner() {
