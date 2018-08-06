813497e94c4aa63023d8c8c6472c83bfc983946b
Lara Sanders <larasanders@gmail.com>
2018-08-06 20:32:23 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,14 +1,10 @@
-// Code your JavaScript / jQuery solution here
+
 const gameWinArray = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
           [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 var currentGame = 0
 var turn = 0
 var player = () => turn % 2 ? 'O' : 'X';
-
-$(document).ready(function() {
-  attachListeners();
-});
 
 function doTurn(square) {
 		updateState(square);
@@ -24,28 +20,37 @@ function doTurn(square) {
 }
 }
 
+$(document).ready(function() {
+  attachListeners();
+});
 
 function attachListeners() {
- 
- // el.addEventListener("click", function(e) {
-  // $('td').text((index, square) => {
- // 	document.querySelectorAll("td").forEach(function(square) {
- // 		square.addEventListener("click", function() {
- // 			doTurn(this)
-	// 	});
-	// 	});
-	
-	$("td").click(function(event) {	
-	 doTurn(event)	
- });
-	var el = document.getElementById("#button");
-	$('#save').on('click', () => saveGame());
-	$('#previous').on('click', () => showPreviousGames());
-	$('#clear').on('click', () => clearBoard());
+
+    $("tbody").click(function(event) {
+      doTurn(square)
+    });
+    $("#save").click(function() {
+  saveGame();
+})
+$("#previous").click(function() {
+  getPreviousGames();
+})
+$("#clear").click(function() {
+  clearBoard();
+})
 }
+
+// 	$("td").click(function(event) {
+// 	 doTurn(event)
+//  });
+// 	var el = document.getElementById("#button");
+// 	$('#save').on('click', () => saveGame());
+// 	$('#previous').on('click', () => showPreviousGames());
+// 	$('#clear').on('click', () => clearBoard());
+// }
 // }
 // })
-// 
+//
 
 // }
 
@@ -55,7 +60,7 @@ function setMessage(message) {
 }
 function updateState(square) {
 	var token = player()
-$(square).text(token)
+$(square).html(token)
 }
 
 function clearBoard() {
@@ -64,8 +69,8 @@ function clearBoard() {
 	currentGame = 0
 }
 function gameId() {
-var getGameId = function(event) {	
-  return $(event.target).data("gameid")	
+var getGameId = function(event) {
+  return $(event.target).data("gameid")
 }
 }
 function saveGame() {
@@ -83,41 +88,32 @@ if (currentGame) {
 	})
 } else {
 	$.post('/games', gameData, function(game) {
-		currentGame = game.data.id;
-		$('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`)
-		$("#gameid-" + game.data.id).on('click', () => populateBoard(arr))
+		currentGame = data.game.id;
+		$('#games').append(`<button id="gameid-${data.game.id}">${data.game.id}</button><br>`)
+		$("#gameid-" + data.game.id).on('click', () => populateBoard(arr))
 	})
 }
 }
-
-function showPreviousGames() {
-	$.get('/games', function(gameId) {
-
-		
-		$('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br>`)
-		$("#gameid-" + game.data.id).on('click', () => populateBoard(arr))
-	})
-
-
-  return $('<li>', {'data-state': game.state, 'data-gameid': game.id, text: game.id});	
+//
+function getPreviousGames() {
+  $.getJSON("/games").done(function(response) {
+  showGames(response.games)
+})
 }
-	// $("#games").click(function(event) {
-	// 	gameId(event)
-	// })
-	// $("#save").click(function() {
-	// 	save()
-	// })
-// 
-// }
-// function showGame(game) {
-// 	return $('<li>', {'data-state': game.state, 'data-gameid': game.id, text: game.id})
-// }
+var showGames = function(games) {
+
+  games.each(function(game) {
+  return $('<li>', {'data-state': game.state, 'data-gameid': game.id, text: game.id});
+})
+}
+//
+
 
 function checkWinner() {
 	var win = {}
 	var winner = false;
  $('td').text((index, square) => win[index] = square);
- 
+
  gameWinArray.some(function(opt) {
 if (win[opt[0]] != "" && win[opt[0]] === win[opt[1]] && win[opt[1]] === win[opt[2]]) {
 message = (`Player ${win[opt[0]]} Won!`)
@@ -127,6 +123,3 @@ return winner = true;
 });
 return winner;
 }
-
-
-
