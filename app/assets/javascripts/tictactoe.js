1cf31135efa4e7a5dd32e437e65d811134b02156
Lara Sanders <larasanders@gmail.com>
2018-08-09 20:34:30 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -54,7 +54,7 @@ return winner;
 
 function attachListeners() {
 $("td").on('click', function() {
-  if (!$.text(this) && !$('td').empty())
+  if (!$.text(this) && !setMessage(message))
   doTurn(this)
   });
 $("#save").click(function() {
